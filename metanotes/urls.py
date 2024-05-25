51e2df1f675204bf2d48be1aa3561cd7bfd42126
Milan Slesarik <milslesarik@gmail.com>
2024-05-26 01:42:23 +0200

    settings: specify app_name

--- metanotes/urls.py
+++ metanotes/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import GetNotesView, AddNoteView, RemoveNoteView
 
+app_name = 'metanotes'
+
 urlpatterns = [
     path('get/', GetNotesView.as_view(), name='get'),
     path('add/', AddNoteView.as_view(), name='add'),
